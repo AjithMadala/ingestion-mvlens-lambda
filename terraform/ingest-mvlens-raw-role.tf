dd47661d2a9d92976b44eccc239638aa3103b43f
AjithMadala <madala.ajith111@gmail.com>
2023-06-05 19:44:03 -0500

    change files

--- terraform/ingest-mvlens-raw-role.tf
+++ terraform/ingest-mvlens-raw-role.tf
@@ -63,20 +63,3 @@ resource "aws_iam_role_policy_attachment" "s3_attachment" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.s3_policy.arn
 }
-
-resource "aws_cloudwatch_event_rule" "schedule_rule" {
-  name        = "movielens-scheduled-rule"
-  description = "Scheduled rule to trigger moviele ns Lambda function"
-
-  schedule_expression = "cron(0 12 * * ? *)"  # Change to your desired schedule
-
-  tags = {
-    Environment = "Production"
-  }
-}
-
-resource "aws_cloudwatch_event_target" "lambda_target" {
-  rule      = aws_cloudwatch_event_rule.schedule_rule.name
-  arn       = aws_lambda_function.lambda-source-raw.arn
-  target_id = "my-lambda-target"
-}
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -16,6 +16,23 @@ resource "aws_lambda_function" "lambda-src-raw" {
   runtime = "python3.10"
 
 
+resource "aws_cloudwatch_event_rule" "schedule_rule" {
+  name        = "movielens-scheduled-rule"
+  description = "Scheduled rule to trigger moviele ns Lambda function"
+
+  schedule_expression = "cron(0 12 * * ? *)"  # Change to your desired schedule
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.schedule_rule.name
+  arn       = aws_lambda_function.lambda-src-raw.arn
+  target_id = "my-lambda-target"
+}
+
   ephemeral_storage {
     size = 512
   }
