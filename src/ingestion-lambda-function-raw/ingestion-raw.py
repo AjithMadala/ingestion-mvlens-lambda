546a06dffdd6c681b413574bec0560739ecdf62b
AjithMadala <madala.ajith111@gmail.com>
2023-06-07 15:56:49 -0500

    complete code

--- src/ingestion-lambda-function-raw/ingestion-raw.py
+++ src/ingestion-lambda-function-raw/ingestion-raw.py
@@ -2,6 +2,15 @@ import json
 from datetime import datetime
 import boto3
 import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=f"%(asctime)s  %(lineno)d    %(levelname)s   %(message)s",
+)
+log = logging.getLogger("Ingest-Raw")
+log.setLevel(logging.INFO)
+log.info("Log message here")
 
 current_date = datetime.now() 
 year = current_date.year
@@ -9,50 +18,82 @@ month = current_date.month
 day = current_date.day
 
 code_bucket = os.environ['codebucket']
-print(code_bucket)
-
+print(codebucket)
 s3 = boto3.resource('s3')
 s3_client=boto3.client('s3')
+sns_client=boto3.client('sns')
 
+def msg(message):     
+    response = sns_client.publish(
+    TopicArn='arn:aws:sns:us-east-2:977746141011:data-pipeline-notfication',
+    Message=message,
+    Subject='Message from Lambda Code',
+    )
+    
 def lambda_handler(event, context):
-    dataset=event.get("dataset")
     
-    response = s3_client.get_object(Bucket=code_bucket, Key=f"{dataset}/config/process_config.json")
-    config_data = response.get('Body').read().decode('utf-8')
-    config_json = json.loads(config_data)
-    
-    source_bucket = config_json.get('source-bucket')
-    source_folder = config_json.get('source-folder')
-    target_bucket = config_json.get('target-bucket')
-    print(source_bucket)
-    
-    response = s3_client.list_objects_v2(Bucket=source_bucket)
-    print(response)
- 
-    file_list = []
- 
-    for obj in response['Contents']:
-        file_name = obj['Key']
-        file_name = file_name.replace(source_folder + '/', '')
-        file_list.append(file_name)
-    print(file_list)
-
-    for file in file_list[1:]:
-        file_part = file.split('.')[0]
-        file_ext = file.split('.')[1]
-        print(file_part)
-        otherkey = f"{source_folder}/{file_part}/year={year}/month={month}/day={day}/{file_part}_{current_date}.{file_ext}"
-        print(otherkey)
-        copy_source = {
-        'Bucket': source_bucket,
-        'Key': f"{source_folder}/{file}"
-             
-        }
-        bucket = s3.Bucket(target_bucket)
-        bucket.copy(copy_source, otherkey)
+    try:
+        dataset=event.get("dataset")
+        log.info(f"{dataset} is key")    
+        response = s3_client.get_object(Bucket=code_bucket, Key=f"{dataset}/config/process_config.json")
+        msg("Bucket and key are Successfully Excecuted")
+    except Exception as e:
+        log.exception(f"error {e}")
+        msg("Error From Key and Bucket {e}")
         
+    try:
+        config_data = response.get('Body').read().decode('utf-8')
+        config_json = json.loads(config_data)    
+        source_bucket = config_json.get('source-bucket')
+        source_folder = config_json.get('source-folder')
+        target_bucket = config_json.get('target-bucket')
+        log.info(source_bucket)
+        msg("Configuring of Bucket is Successful")
+        
+    except Exception as e:
+        log.exception(f"error {e}")
+        msg(f"Error from configuring bucket {e}")
+        
+    try:    
+        response = s3_client.list_objects_v2(Bucket=source_bucket)
+        log.info(response)
+
+        file_list = []
+
+        for obj in response['Contents']:
+            file_name = obj['Key']
+            file_name = file_name.replace(source_folder + '/', '')
+            file_list.append(file_name)
+        log.info(file_list)
+
+        for file in file_list[1:]:
+            file_part = file.split('.')[0]
+            file_ext = file.split('.')[1]
+            log.info(file_part)
+            otherkey = f"{source_folder}/{file_part}/year={year}/month={month}/day={day}/{file_part}_{current_date}.{file_ext}"
+            log.info(otherkey)
+            
+            copy_source = {
+            'Bucket': source_bucket,
+            'Key': f"{source_folder}/{file}"
+                
+            }
+            bucket = s3.Bucket(target_bucket)
+            bucket.copy(copy_source, otherkey)
+            msg("Processing Files is Successful")
+       
+    except Exception as e:
+        log.exception(f"error {e}")
+        msg(f"Error from files processing {e}")
+        
+        response = sns_client.publish(
+        TopicArn='arn:aws:sns:us-east-2:977746141011:data-pipeline-notfication',
+        Message='Successfully Ingested Raw Data',
+        Subject='Ingest Raw Data',)
+        
+
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-             
+        'body': json.dumps(otherkey)
         }
--- terraform/ingest-mvlens-raw-role.tf
+++ terraform/ingest-mvlens-raw-role.tf
@@ -43,6 +43,20 @@ data "aws_iam_policy_document" "s3_policy" {
   }
 }
 
+data "aws_iam_policy_document" "sns_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sns:*"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
@@ -62,4 +76,34 @@ resource "aws_iam_role_policy" "cloud_watch_policy" {
 resource "aws_iam_role_policy_attachment" "s3_attachment" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+
+resource "aws_cloudwatch_event_rule" "schedule_rule" {
+  name        = "movielens-scheduled-rule"
+  description = "Scheduled rule to trigger moviele ns Lambda function"
+
+  schedule_expression = "cron(0 12 * * ? *)"  # Change to your desired schedule
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.schedule_rule.name
+  arn       = aws_lambda_function.lambda-src-raw.arn
+  target_id = "my-lambda-target"
+}
+
+resource "aws_iam_role_policy" "sns_policy_attachment" {
+  name   = "sns_policy_attachment"
+  policy = data.aws_iam_policy_document.sns_policy.json
+  role   = aws_iam_role.iam_for_lambda.name
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = "arn:aws:sns:us-east-2:977746141011:data-pipeline-notfication"
+  protocol  = "email-json"
+  endpoint  = "madala.ajith111@gmail.com"
 }
